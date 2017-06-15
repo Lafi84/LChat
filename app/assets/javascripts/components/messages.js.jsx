e8aa83aab172c9982d5b540c5683390bfcc1b6ec
Lafi84 <Lafi84@gmail.com>
2017-06-15 21:40:19 +0300

    Added google geolocation api. Fixed a couple of bugs

--- app/assets/javascripts/components/messages.js.jsx
+++ app/assets/javascripts/components/messages.js.jsx
@@ -24,21 +24,23 @@ class Messages extends React.Component {
         this.handleNewMessage = this.handleNewMessage.bind(this);
         this.fetchComments = this.fetchComments.bind(this);
         this.updateLocation = this.updateLocation.bind(this);
+        this.updateGeoLocation = this.updateGeoLocation.bind(this);
         this.setLocation = this.setLocation.bind(this);
         this.setMockLocation = this.setMockLocation.bind(this);
+        this.doMapsApiGeolocation = this.doMapsApiGeolocation.bind(this);
     }
 
     componentDidMount() {
         this.updateGeoLocation();
-        // var intervalId = setInterval(this.fetchComments, 10000);
-
-        // this.setState({intervalId: intervalId});
     }
 
 
     updateGeoLocation() {
         console.log("Setting geolocation watcher");
-        navigator.geolocation.watchPosition((pos) =>
+
+        this.setState({locationError: undefined});
+
+        var _nav = navigator.geolocation.watchPosition((pos) =>
             {
                 var crd = pos.coords;//{lat: pos.coords.latitude, lon: pos.coords.longitude, accuracy: pos.coords.latitude.accuracy};
 
@@ -52,10 +54,45 @@ class Messages extends React.Component {
                 this.updateLocation(crd);
             },
             (error) =>{
-                this.setState({locationError: 'Error while trying to get location: '});
+                // this.setState({locationError: 'Error while trying to get location: '});
+
+                navigator.geolocation.clearWatch(_nav);
+                switch (error.code) {
+                    case error.TIMEOUT:
+                        this.setState({locationError: 'Geolocation timeout'});
+                        break;
+                    // case error.PERMISSION_DENIED:
+                    //     this.doMapsApiGeolocation();
+                    //     break;
+                    case error.POSITION_UNAVAILABLE:
+                        this.setState({locationError: 'Geolocation error. Position unavailable'});
+                        break
+
+                    default:
+                        this.setState({locationError: 'Geolocation error. Position unavailable'});
+                        break;
+                }
             }
         );
     }
+
+    doMapsApiGeolocation() {
+        $.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB34B23BRTnK1y1LlTruuzCHyEITyMFPKE", (success) => {
+                var _pos = {latitude: success.location.lat, longitude: success.location.lng, accuracy: success.location.accuracy};
+
+                console.log('Position updated with google maps api:');
+                console.log(`Latitude : ${_pos.latitude}`);
+                console.log(`Longitude: ${_pos.longitude}`);
+                console.log(`More or less ${_pos.accuracy} meters.`);
+
+                this.setState({locationError: undefined});
+
+                this.updateLocation(_pos);
+            })
+            .fail(function(err) {
+                this.setState({locationError: "Geolocation failed"});
+            });
+    };
 
     updateLocation(coords) {
         console.log("Fetching updated location with coordinates lat:"+ coords.latitude +",lng:"+ coords.longitude);
@@ -64,7 +101,7 @@ class Messages extends React.Component {
             if(location.geonames && location.geonames[0].name){
                 var _location = location.geonames[0].name;
 
-                if(this.state.location != _location) {
+                if(this.state.location.name != _location) {
                     console.log("Updating location to: " + _location);
                     this.setLocation(_location);
                 }else{
@@ -77,13 +114,15 @@ class Messages extends React.Component {
     setLocation(locationName) {
         this.state.location.name = locationName;
         this.state.location.location_id = undefined;
-        //this.setState({location: {name: _location, location_id: undefined}});
         this.fetchComments(true);
     }
 
     componentWillUnmount() {
         if(this.state.intervalId)
-            clearInterval(this.state.intervalId);
+            clearInterval(this.state.intervalId)
+
+        if(_nav)
+            navigator.geolocation.clearWatch(_nav);
     }
 
     fetchComments(clearComments) {
@@ -103,12 +142,14 @@ class Messages extends React.Component {
             $.get('/locations?location_id='+location_id, (data) =>{
                 if(location_id===_instance.state.location.location_id) {
                     var _currentMessages = _instance.state.messages;
+                    var _foundOne = false;
 
                     data.forEach(function (_newMessage) {
                         var _found = false;
                         for (var _i in _currentMessages) {
                             if (_currentMessages[_i].id === _newMessage.id) {
                                 _found = true;
+                                _foundOne = true;
                                 break;
                             }
                         }
@@ -117,6 +158,10 @@ class Messages extends React.Component {
                         }
                     });
                     _instance.setState({messages: _currentMessages, fetchingMessages: false});
+
+                    if(clearComments || _foundOne){
+                        _instance.scrollToBottom();
+                    }
                 }
             }, 'JSON');
         };
@@ -128,7 +173,6 @@ class Messages extends React.Component {
                 var _currentLoc = this.state.location;
                 _currentLoc.location_id = data.id;
                 this.state.location.location_id = data.id;
-                // this.setState({location: _currentLoc});
                 _getComments(data.id);
             });
         }
@@ -142,6 +186,12 @@ class Messages extends React.Component {
     setMockLocation() {
         this.setState({locationError: undefined});
         this.setLocation("Kaijonharju");
+    }
+
+    scrollToBottom() {
+        const node = ReactDOM.findDOMNode(this.messagesEnd);
+        if(node)
+            node.scrollIntoView({ behavior: "smooth" });
     }
 
     //type="submit"
@@ -172,7 +222,7 @@ class Messages extends React.Component {
                         <div className="navbar-header">
                             <a className="navbar-brand" href="#">
                                 LChat
-                                {this.state.location.name ? ' Location: ' + this.state.location.name : ''}
+                                {this.state.location.name ? ' ' + this.state.location.name : ''}
                                 {this.state.location.location_id ? ' (' + this.state.location.location_id + ')': ''}
                             </a>
                         </div>
@@ -183,7 +233,7 @@ class Messages extends React.Component {
                         <strong>Error</strong> <br/> {this.state.error}
                     </div> : ''}
                     {this.state.locationError ? <div className="alert alert-danger">
-                        <strong>Location Error</strong> <br/> {this.state.error} <a onClick={this.setMockLocation}>Use Kaijonharju</a>
+                        <strong>Location Error</strong> <br/> {this.state.error} <a onClick={this.updateGeoLocation}>Try again</a> or <a onClick={this.setMockLocation}>Use Kaijonharju</a>
                     </div> : ''}
 
                     {this.state.location.location_id ?
@@ -203,7 +253,9 @@ class Messages extends React.Component {
                                     </p>
                                 </div>
                             </li>
-                        )}</ul>
+                        )}
+                            <div style={{ float:"left", clear: "both" }} ref={(el) => { this.messagesEnd = el; }}></div>
+                        </ul>
                          : !this.state.error ? <div className="loader">Loading Location...</div> : ''}
                 </div>
                     {React.createElement(MessageInput, {handleAddMessage: this.handleNewMessage, sender: this.state.sender, location: this.state.location}, null)}
